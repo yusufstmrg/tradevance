10245512d6d70d370a4cdf9759555928fa95ba72
Yusuf B. Situmorang <yusufbsitumorang@gmail.com>
2026-09-22 20:28:30 +0700

    sync: AppDeploy v84 Vite configuration

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [react()],
+    base: './',
+    build: {
+        outDir: process.env.APPDEPLOY_VITE_OUT_DIR || 'dist',
+        sourcemap: process.env.APPDEPLOY_VITE_SOURCEMAP === 'hidden' ? 'hidden' : false,
+        rollupOptions: { maxParallelFileOps: 128 },
+    },
+});
